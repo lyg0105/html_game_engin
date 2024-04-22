3ed4bf17be25c1bc2e319c28392e4a40b4a5f550
lyg0105 <lyg0105@naver.com>
2024-04-22 22:50:45 +0900

    game base

--- test/char_move_test/public/js/event/controler.js
+++ test/char_move_test/public/js/event/controler.js
@@ -14,17 +14,30 @@ class Controler
     
   }
   init(){
-
+    let body_obj=document.getElementsByTagName("BODY")[0];
+    let this_obj=this;
+    body_obj.addEventListener("keydown", (e)=>{
+      this_obj.up(e.keyCode);
+    });
+    body_obj.addEventListener("keyup", (e)=>{
+      this_obj.down(e.keyCode);
+    });
   }
   up(key_code){
     this.key_state[key_code]=false;
+    this.on_change_key();
   }
   down(key_code){
     this.key_state[key_code]=true;
+    this.on_change_key();
   }
   all_up(){
     for(let key in this.this.key_state){
       this.key_state[key]=false;
     }
+    this.on_change_key();
   }
+  on_change_key=()=>{
+    
+  };
 }
--- test/char_move_test/public/js/game/main.js
+++ test/char_move_test/public/js/game/main.js
@@ -5,6 +5,15 @@ class GameMain
   map=null;
 
   init(){
-    
+    let canvas_wrap=document.getElementById("canvas_wrap");
+    let screen_w = canvas_wrap.offsetWidth;
+    let screen_h = canvas_wrap.offsetHeight;
+    this.canvas = MakeCanvas.make("canvas_wrap");
+    const ctx = this.canvas.getContext("2d");
+    this.map = new Map(2000, 2000, screen_w, screen_h, ctx);
+    this.map.draw();
+
+    this.controller=new Controler();
+    this.controller.init();
   }
 }
